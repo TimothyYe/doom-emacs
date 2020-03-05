06f69346070e23726f9a1b3e36551609f78f7a26
Timothy <yexiaozhou2003@gmail.com>
2020-03-05 15:09:24 +0800

    update window split settings

--- keybindings/+keybindings.el
+++ keybindings/+keybindings.el
@@ -4,6 +4,7 @@
 (map! :leader
       (:prefix "w"
         :desc "evil-window-vsplit" "/" #'evil-window-vsplit
+        :desc "evil-window-split" "_" #'evil-window-split
         )
       (:prefix "a"
         :desc "open elfeed" "f" #'elfeed
