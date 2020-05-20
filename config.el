c6dd643ad77bd77aed5d9017110dd626069ce453
Timothy <yexiaozhou2003@gmail.com>
2020-05-20 21:15:29 +0800

    add helm-spotify-plus

--- config.el
+++ config.el
@@ -20,4 +20,4 @@
 ;;
 ;; You can also try 'gd' (or 'C-c g d') to jump to their definition and see how
 ;; they are implemented.
-;;
+(use-package! helm-spotify-plus)
--- keybindings/+keybindings.el
+++ keybindings/+keybindings.el
@@ -9,4 +9,19 @@
       (:prefix "a"
         :desc "open elfeed" "f" #'elfeed
         )
+      (:prefix "a"
+        :desc "helm-spotify-plus" "s" #'helm-spotify-plus
+        )
+      (:prefix "a"
+        :desc "helm-spotify-plus-next" "n" #'helm-spotify-plus-next
+        )
+      (:prefix "a"
+        :desc "helm-spotify-plus-previous" "b" #'helm-spotify-plus-previous
+        )
+      (:prefix "a"
+        :desc "helm-spotify-plus-next" "p" #'helm-spotify-plus-play
+        )
+      (:prefix "a"
+        :desc "helm-spotify-plus-pause" "g" #'helm-spotify-plus-pause
+        )
       )
